2207f47dc39e0d91f36e1c48cdde72c56b612076
Daniel Palencia <dpalencia2112@gmail.com>
2019-08-12 08:26:09 -0700

    useEffect

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,17 +2,21 @@ import React, {useState, useEffect} from "react"
 import WContainer from "./WeatherBoxes/WContainer"
 
 function MainContainer() {
-    let responseJSON = {testing: "Testing"}
+    
+    const [response, setResponse] = useState({})
     useEffect(function() {
+        /*
         fetch('https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0')
         .then((response) => response.json())
-        .then(function(json) {responseJSON = json})
-    }, [responseJSON]);
-    console.log(responseJSON)
+        .then(json => setResponse(json))*/
+        console.log("Hello!")
+    }, [response]);
+
     return (
         <div id="mainContainer">
             <div id="mainTitle">
                 <h1>7/20/19 - Sol 233</h1>
+
             </div>
             <WContainer />
         </div>
