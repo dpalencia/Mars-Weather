a6a7ddb8f3668334eac96c405993551faf6ef1e0
Daniel Palencia <dpalencia2112@gmail.com>
2019-08-12 13:33:43 -0700

    Weather Data API

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,21 +2,18 @@ import React, {useState, useEffect} from "react"
 import WContainer from "./WeatherBoxes/WContainer"
 
 function MainContainer() {
-    
-    const [response, setResponse] = useState({})
+    const [weatherData, setResponse] = useState(null)
     useEffect(function() {
-        /*
         fetch('https://mars.nasa.gov/rss/api/?feed=weather&category=insight&feedtype=json&ver=1.0')
         .then((response) => response.json())
-        .then(json => setResponse(json))*/
-        console.log("Hello!")
-    }, [response]);
-
+        .then(json => setResponse(json))
+    }, []);
+    
     return (
         <div id="mainContainer">
             <div id="mainTitle">
                 <h1>7/20/19 - Sol 233</h1>
-
+                {weatherData ? weatherData[245]['AT']['av'] : ''}
             </div>
             <WContainer />
         </div>
