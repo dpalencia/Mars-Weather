889b0d166cf118c5a970183fee4074ae13533a88
Daniel Palencia <dpalencia2112@gmail.com>
2019-08-13 08:24:56 -0700

    Unit conversion work

--- src/components/UnitButton.js
+++ src/components/UnitButton.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function UnitButton(props) {
     return(
-        <button onClick={props.handler}>
+        <button onClick={props.handler} value={props.text}>
             {props.text}
         </button>
     )
--- src/components/WeatherBoxes/WContainer.js
+++ src/components/WeatherBoxes/WContainer.js
@@ -8,22 +8,25 @@ import UnitButton from "../UnitButton"
 function WeatherBoxes(props) {
     let wind = props.weatherVals[props.day].HWS
     let pressure = props.weatherVals[props.day].PRE
+
+    const ctoF = (temp) => temp * 1.8 + 32
+    const ctoK = (temp) => temp + 273.15
+
+
     const [temp, setTemp] = useState(
         {
-            unit: "F",
-            av: Math.ceil(props.weatherVals[props.day].AT.av),
-            mn: Math.ceil(props.weatherVals[props.day].AT.mn),
-            mx: Math.ceil(props.weatherVals[props.day].AT.mx)
+            unit: "C",
+            av: Math.round(props.weatherVals[props.day].AT.av),
+            mn: Math.round(props.weatherVals[props.day].AT.mn),
+            mx: Math.round(props.weatherVals[props.day].AT.mx)
         }
     )
-    function tempConversion(e) {
-        if(e.value === "F") {
-            setTemp(prevTemp => ({...prevTemp, ...{unit: "C"}}))
-        }
-        else {
-            setTemp(prevTemp => ({...prevTemp, ...{unit: "F"}}))
-        }
+    function tempHandler(e) {
+        /* Maybe do this conversion cleaner by using array map */
+        e.persist()
+        setTemp(tempConvert())
     }
+
     return (
         <div id="wGrid">
             <WeatherBox title="Temperature" gridName="temp">
@@ -38,8 +41,8 @@ function WeatherBoxes(props) {
                 />
                 <div id="scaleContainer">
                 <h4>Scale</h4>
-                <UnitButton text="F" handler={tempConversion} />
-                <UnitButton text="C" handler={tempConversion} />
+                <UnitButton text="F" handler={tempHandler} />
+                <UnitButton text="C" handler={tempHandler} />
             </div>
             </WeatherBox>
             <WeatherBox title="Pressure" gridName="pressure">
