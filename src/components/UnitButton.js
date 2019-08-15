76c8be7a577d4559cf1336a66593334f7e51124d
Daniel Palencia <dpalencia2112@gmail.com>
2019-08-14 23:12:05 -0700

    Unit conversion functionality

--- src/components/UnitButton.js
+++ src/components/UnitButton.js
@@ -2,7 +2,11 @@ import React from "react"
 
 function UnitButton(props) {
     return(
-        <button onClick={props.handler} value={props.text}>
+        <button 
+            onClick={props.handler} 
+            value={props.text} 
+            className={`unitbtn${props.currentUnit === props.text ? " selected" : ""}`}
+        >
             {props.text}
         </button>
     )
--- src/components/WeatherBoxes/WContainer.js
+++ src/components/WeatherBoxes/WContainer.js
@@ -3,68 +3,117 @@ import WeatherBox from "./WeatherBox"
 import InfoValues from "./InfoValues"
 import UnitButton from "../UnitButton"
 
-
-
 function WeatherBoxes(props) {
-    let wind = props.weatherVals[props.day].HWS
-    let pressure = props.weatherVals[props.day].PRE
+    /* Defining these vars to make later data access easier */
+    let jsonTemp = props.weatherVals[props.day].AT
+    let jsonWind = props.weatherVals[props.day].HWS
+    let jsonPressure = props.weatherVals[props.day].PRE
 
-    const ctoF = (temp) => temp * 1.8 + 32
-    const ctoK = (temp) => temp + 273.15
+    let tempData = [
+        jsonTemp.mx,
+        jsonTemp.mn,
+        jsonTemp.av
+    ]
+    let windData = [
+        jsonWind.mx,
+        jsonWind.mn,
+        jsonWind.av
+    ]
 
+    let pressureData = [
+        jsonPressure.mx,
+        jsonPressure.mn,
+        jsonPressure.av
+    ]
 
-    const [temp, setTemp] = useState(
-        {
-            unit: "C",
-            av: Math.round(props.weatherVals[props.day].AT.av),
-            mn: Math.round(props.weatherVals[props.day].AT.mn),
-            mx: Math.round(props.weatherVals[props.day].AT.mx)
-        }
-    )
-    function tempHandler(e) {
-        /* Maybe do this conversion cleaner by using array map */
-        e.persist()
-        setTemp(tempConvert())
+    let tempFuncs = {
+        C: () => tempData.map((num) => Math.round(num)),
+        F: () => tempData.map((num) => Math.round(num * 1.8 + 32)), // C -> F conversion 
+        K: () => tempData.map((num) => Math.round(num + 273.15)) // C -> K conversion
     }
+
+    let windFuncs = {
+        mph: () => windData.map((num) => Math.round(num * 2.237)),
+        ["m/s"]: () => windData.map(num => Math.round(num)) // Miles/hour -> Meters/second conversion
+    }
+
+    const [temp, setTemp] = useState(tempFuncs.C())
+    const [tempUnit, setTempUnit] = useState("C")
+    const [windUnit, setWindUnit] = useState("m/s")
+    const [wind, setWind] = useState(windFuncs["m/s"]())
+
+    // Don't need to manage state for pressure, so we can just make it a regular variable
+    let pressure = pressureData.map(num => Math.round(num))
+
+    function tempHandler(e) {
+        e.persist()
+        setTempUnit(e.target.value)
+        setTemp(tempFuncs[e.target.value])
+    }
+
+    function windHandler(e) {
+        e.persist()
+        setWindUnit(e.target.value)
+        setWind(windFuncs[e.target.value])
+    }
+
 
     return (
         <div id="wGrid">
+
             <WeatherBox title="Temperature" gridName="temp">
+
                 <InfoValues
                   maxName="High"
                   minName="Low" 
                   avName="Average" 
-                  maxVal={temp.mx}
-                  minVal={temp.mn}
-                  avVal={temp.av}
-                  unit={temp.unit}
+                  maxVal={temp[0]}
+                  minVal={temp[1]}
+                  avVal={temp[2]}
+                  unit={tempUnit}
                 />
                 <div id="scaleContainer">
                 <h4>Scale</h4>
-                <UnitButton text="F" handler={tempHandler} />
-                <UnitButton text="C" handler={tempHandler} />
+                <UnitButton text="F" handler={tempHandler} currentUnit={tempUnit} />
+                <UnitButton text="C" handler={tempHandler} currentUnit={tempUnit} />
+                <UnitButton text="K" handler={tempHandler} currentUnit={tempUnit} />
             </div>
             </WeatherBox>
+
             <WeatherBox title="Pressure" gridName="pressure">
                 <InfoValues
                     maxName="Max"
                     minName="Min"
                     avName="Average"
-                    maxVal={pressure.mx}
-                    minVal={pressure.mn}
-                    avVal={pressure.av}
+                    maxVal={pressure[0]}
+                    minVal={pressure[1]}
+                    avVal={pressure[2]}
+                    unit="Pa"
                 />
             </WeatherBox>
+
             <WeatherBox title="Wind Speed" gridName="weather">
                 <InfoValues
                     maxName="Max"
                     minName="Min"
                     avName="Average"
-                    maxVal={wind.mx}
-                    minVal={wind.mn}
-                    avVal={pressure.av}    
+                    maxVal={wind[0]}
+                    minVal={wind[1]}
+                    avVal={wind[2]}   
+                    unit={windUnit} 
+                />
+                <UnitButton 
+                    text="mph" 
+                    handler={windHandler}
+                    currentUnit={windUnit}
+                />
+                <UnitButton 
+                    text="m/s" 
+                    handler={windHandler}
+                    currentUnit={windUnit}
                 />
             </WeatherBox>
+
          </div>
     )
 }
